77cfe79783aabc5d8ec9fa32deab0a622691b79a
tkolecki <tkolecki@gmail.com>
2026-05-01 21:20:40 +0200

    Add docstrings to functions

--- calculator.py
+++ calculator.py
@@ -1,19 +1,24 @@
 def add(a, b):
+    ''' Sums a and b. Returns sum '''
     return a + b
 
 def subtract(a, b):
+    ''' Subtract a and b. Returns difference '''
     return a - b
 
 def multiply(a, b):
+    ''' Multiply a and b. Returns multiplication '''
     return a * b
 
 def divide(a, b):
+    ''' Divide a and b. Returns error when b equal 0 '''
     if b == 0:
         return "Error: cannot divide by 0"
     return a / b
 
 def power(a, b):
-    return pow(a,b)
+    ''' Raise a to a power of b. Returns exponentation '''
+    return pow(a, b)
 
 def get_number(prompt):
     while True:
@@ -44,19 +49,20 @@ def main():
     history = []
     print("--- CALCULATOR ---")
     while True:
-        a = get_number("a =")
+        a = get_number("a = ")
         o = get_operation()
         if o == "q":
             break
-        b = get_number("b =")
+        b = get_number("b = ")
         result = f"Result: {a} {o} {b} = {calculate(a, o, b)}"
         history.append(result)
         print(result)
         print("\n History:")
         for i, v in enumerate(history[-5:]):
-            print(f"{i+1}: {v}")
+            print(f"{i + 1}: {v}")
 
-main()
+if __name__ == "__main__":
+    main()
 # 
 # main()tests
 # print(add(2, 3))        # 5
